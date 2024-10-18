790f622ef489c1e5251e843d6edd3e41979a453e
Raphael Meyer <raphael.akira.meyer@gmail.com>
2024-10-18 19:27:27 +0200

    score hikari cards

--- backend/src/KoiKoi/Game.hs
+++ backend/src/KoiKoi/Game.hs
@@ -10,10 +10,26 @@ newtype Pile = Pile {unPile :: Set.Set Cards.Card} deriving (Eq, Show)
 emptyPile :: Pile
 emptyPile = Pile Set.empty
 
-makePile :: [Cards.Card] -> Pile
+makePile :: [Card] -> Pile
 makePile = Pile . Set.fromList
 
 score :: Pile -> Int
-score pile = if kasu >= 10 then kasu - 9 else 0
+score pile = scoreHikari cards + scoreKasu cards
   where
-    kasu = length . withPoints Cards.Kasu . Set.toList . unPile $ pile
+    cards = Set.toList . unPile $ pile
+
+scoreHikari :: [Card] -> Int
+scoreHikari cards = case (length hikari, raining) of
+  (5, _) -> 10
+  (4, False) -> 8
+  (4, True) -> 7
+  (3, False) -> 5
+  _ -> 0
+  where
+    hikari = withPoints Hikari cards
+    raining = not . null . withName Michikaze $ hikari
+
+scoreKasu :: [Card] -> Int
+scoreKasu cards = if kasu >= 10 then kasu - 9 else 0
+  where
+    kasu = length . withPoints Kasu $ cards
--- backend/test/KoiKoi/GameSpec.hs
+++ backend/test/KoiKoi/GameSpec.hs
@@ -22,6 +22,63 @@ spec = do
       it "scores one point for ten cards" $ do
         (Game.score . Game.makePile . take 10 $ kasu) `shouldBe` 1
 
-      it "scores on additional point for each additional card" $ do
+      it "scores one additional point for each additional card" $ do
         (Game.score . Game.makePile . take 11 $ kasu) `shouldBe` 2
         (Game.score . Game.makePile . take 14 $ kasu) `shouldBe` 5
+
+    describe "hikari" $ do
+      it "scores ten points for all five hikari cards" $ do
+        let pile =
+              Game.makePile
+                [ makeCard Matsu Hikari Tsuru,
+                  makeCard Sakura Hikari Mankai,
+                  makeCard Susuki Hikari Mochiduki,
+                  makeCard Yanagi Hikari Michikaze,
+                  makeCard Kiri Hikari Houou
+                ]
+        Game.score pile `shouldBe` 10
+
+      it "scores eight points for four hikari cards excluding Ono no Michikaze" $ do
+        let pile =
+              Game.makePile
+                [ makeCard Matsu Hikari Tsuru,
+                  makeCard Sakura Hikari Mankai,
+                  makeCard Susuki Hikari Mochiduki,
+                  makeCard Kiri Hikari Houou
+                ]
+        Game.score pile `shouldBe` 8
+
+      it "scores seven points for four hikari cards including Ono no Michikaze" $ do
+        let pile =
+              Game.makePile
+                [ makeCard Matsu Hikari Tsuru,
+                  makeCard Susuki Hikari Mochiduki,
+                  makeCard Yanagi Hikari Michikaze,
+                  makeCard Kiri Hikari Houou
+                ]
+        Game.score pile `shouldBe` 7
+
+      it "scores five points for three hikari cards excluding Ono no Michikaze" $ do
+        let pile =
+              Game.makePile
+                [ makeCard Matsu Hikari Tsuru,
+                  makeCard Sakura Hikari Mankai,
+                  makeCard Kiri Hikari Houou
+                ]
+        Game.score pile `shouldBe` 5
+
+      it "scores no points for other combinations" $ do
+        let pile =
+              Game.makePile
+                [ makeCard Sakura Hikari Mankai,
+                  makeCard Yanagi Hikari Michikaze,
+                  makeCard Kiri Hikari Houou
+                ]
+        Game.score pile `shouldBe` 0
+
+    describe "whole pile" $ do
+      it "should add up all valid scores" $ do
+        let kasu = withPoints Kasu deck
+        let hikari = withPoints Hikari deck
+        let pile = Game.makePile $ take 12 kasu ++ hikari
+        Game.score pile `shouldBe` 13
